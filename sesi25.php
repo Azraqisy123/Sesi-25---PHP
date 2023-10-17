1577126dea321a77050169e60cb3158a2615d33a
Azraqisy123 <107374487+Azraqisy123@users.noreply.github.com>
2023-10-17 21:12:10 +0700

    second commit

---
+++ sesi25.php
@@ -0,0 +1,21 @@
+<?php
+    //soal nomor 1
+    $bilangan = 10;
+
+    if ($bilangan % 2 == 0) {
+        echo "angka $bilangan merupakan bilangan genap";
+    } 
+    else {
+        echo "angka $bilangan merupakan bilangan ganjil";
+    } 
+
+    //soal nomor 2
+    $tahun = 2023;
+
+    if ($tahun % 4 == 0) {
+        echo "tahun $tahun merupakan tahun kabisat";
+    } 
+    else {
+        echo "tahun $tahun merupakan tahun kabisat";
+    }
+?>
