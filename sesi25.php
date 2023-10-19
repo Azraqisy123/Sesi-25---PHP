e286a8f85cc6e6af71430ad59d2c426ac6478a75
Azraqisy123 <107374487+Azraqisy123@users.noreply.github.com>
2023-10-19 20:58:22 +0700

    sesi 26

--- sesi25.php
+++ sesi25.php
@@ -3,19 +3,19 @@
     $bilangan = 10;
 
     if ($bilangan % 2 == 0) {
-        echo "angka $bilangan merupakan bilangan genap";
+        echo "angka $bilangan merupakan bilangan genap <br>";
     } 
     else {
-        echo "angka $bilangan merupakan bilangan ganjil";
+        echo "angka $bilangan merupakan bilangan ganjil <br>";
     } 
 
     //soal nomor 2
     $tahun = 2023;
 
     if ($tahun % 4 == 0) {
-        echo "tahun $tahun merupakan tahun kabisat";
+        echo "tahun $tahun merupakan tahun kabisat <br>";
     } 
     else {
-        echo "tahun $tahun merupakan tahun kabisat";
+        echo "tahun $tahun bukan merupakan tahun kabisat <br>";
     }
 ?>
---
+++ sesi26.php
@@ -0,0 +1,32 @@
+<?php
+    echo "Soal Nomor 1 <br>";
+    for ($i=1; $i <= 10 ; $i++) { 
+        if ($i % 2 == 0) {
+            echo "angka $i merupakan bilangan genap <br>";
+        }
+        else {
+            echo "angka $i merupakan bilangan ganjil <br>";
+        } 
+    }
+    echo "<br>";
+
+    echo "Soal Nomor 2 <br>";
+    for ($a=2000; $a <=2023 ; $a++) { 
+        if ($a % 4 == 0) {
+            echo "Tahun $a adalah tahun kabisat <br>";
+        }
+        else {
+            echo "Tahun $a bukan tahun kabisat <br>";
+        } 
+    }
+    echo "<br>";
+
+    echo "Soal Nomor 3 <br>";
+    for ($i=1; $i <10 ; $i++) {
+        for ($a=10-$i; $a >0 ; $a--) { 
+            echo "$a";
+        } 
+        echo "<br>";
+        
+    }
+?>
