461d8e33c7541dae5a78c0e0841e32b3b270a0b2
Azraqisy123 <107374487+Azraqisy123@users.noreply.github.com>
2023-10-20 20:05:08 +0700

    sesi 27

---
+++ sesi27.php
@@ -0,0 +1,39 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+    echo "Soal Nomor 1 <br>";
+    for ($i=0; $i <=9 ; $i++) {
+        for ($a=0; $a <$i ; $a++) { 
+            echo "$a";
+        } 
+        echo "<br>";
+        
+    }
+    echo "<br>";
+    echo "Soal Nomor 2";
+?>
+    <table border = "1">
+        <tr>
+            <td>No</td>
+            <td>Nama</td>
+            <td>Kelas</td>
+        </tr>
+        <?php
+            for ($i=1; $i <10 ; $i++) { 
+        ?>
+            <tr>
+                <td>No <?php echo "$i" ?></td>
+                <td>Name ke <?php echo "$i" ?></td>
+                <td>Kelas</td>
+            </tr>
+        <?php
+            }
+        ?>
+    </table>
+</body>
+</html>
